87c0b0beacf0a291d814c21d7e0f61708f3c6f6c
Mark Gerard Sangan <sangan.markgerard@ue.edu.ph>
2023-12-09 22:43:39 +0800

    adding

--- register_stud.php
+++ register_stud.php
@@ -1,4 +1,20 @@
 <?php
-$f_name = $_POST ['f_name'];
-$surname = $_POST ["last_name"];
-print_r($_POST);
+
+$dsn = "mysql:host=localhost;dbname=register_stud_db";
+$dbusername = "root";
+$dbpassword = "";
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "connection failed: " . $e->getMessage();
+}
+
+$l_name = $_POST["last_name"];
+$f_name = $_POST["first_name"];
+$m_i = $_POST["middle_initial"];
+$stud_id = $_POST["student_no"];
+$program = $_POST["program"];
+$year = $_POST["year"];
+$email = $_POST["E-mail"];
+$phone_num = $_POST["number"];
