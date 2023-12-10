efe2b92a168919272c27246fac130d2667e07fc6
Mark Gerard Sangan <sangan.markgerard@ue.edu.ph>
2023-12-10 22:42:29 +0800

    ayan

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri);
-	exit;
+    if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+        $uri = 'https://';
+    } else {
+        $uri = 'http://';
+    }
+    $uri .= $_SERVER['HTTP_HOST'];
+    header('Location: index.html');
+    exit;
 ?>
