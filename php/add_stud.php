f8b684782f352f44a789049392ebe1c7778136cc
Mark Gerard Sangan <sangan.markgerard@ue.edu.ph>
2023-12-10 22:53:59 +0800

    up

--- php/add_stud.php
+++ php/add_stud.php
@@ -14,7 +14,7 @@ $conn = new mysqli('localhost', 'root', '', 'register_stud_db');
 if ($conn->connect_error) {
     die('Connection Failed : ' . $conn->connect_error);
 } else {
-    $stmt = $conn->prepare("INSERT INTO register_stud_db (stud_id, l_name, f_name, m_i, program, year_l, email, phone_num) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO student (stud_id, l_name, f_name, m_i, program, year_l, email, phone_num) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
     
     if (!$stmt) {
         die('Error in preparing statement: ' . $conn->error);
