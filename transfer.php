7c0c30b09b3dee2379c226df3b2f5cd76dd2b969
Mark Gerard Sangan <sangan.markgerard@ue.edu.ph>
2023-12-12 16:05:52 +0800

    add nyo to

---
+++ transfer.php
@@ -0,0 +1,29 @@
+<?php
+// Database Connection
+$conn = new mysqli('localhost', 'root', '', 'register_stud_db');
+if ($conn->connect_error) {
+    die('Connection Failed : ' . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $stud_id = $_POST["stud_id"];
+
+    // Inserting data into the new table
+    $insertQuery = "INSERT INTO newtablename (l_name, f_name) SELECT l_name, f_name FROM student WHERE stud_id = ?";
+    $stmt = $conn->prepare($insertQuery);
+
+    if (!$stmt) {
+        die('Error in preparing statement: ' . $conn->error);
+    }
+
+    $stmt->bind_param("i", $stud_id);
+
+    if (!$stmt->execute()) {
+        die('Error in executing statement: ' . $stmt->error);
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
